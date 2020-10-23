b64e654db154a46de189221fa62ed65bc7dca48f
Valentyn Stepanov <falken.ua@gmail.com>
2020-10-23 13:20:01 +0300

    - remove useless video page

--- src/Pixama.Logic/ViewModels/Shell/ShellViewModel.cs
+++ src/Pixama.Logic/ViewModels/Shell/ShellViewModel.cs
@@ -1,70 +1,21 @@
-﻿using System.Collections.ObjectModel;
-using System.Reactive;
-using System.Reactive.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Pixama.Logic.Services;
 using Pixama.Logic.ViewModels.Photo;
-using ReactiveUI;
 
 namespace Pixama.Logic.ViewModels.Shell
 {
     public class ShellViewModel : BaseViewModel
     {
-        #region Fields
-
-        private ShellMenuItemViewModel _selectedTab;
         private readonly INavigationService _navigation;
-
-        private readonly ShellMenuItemViewModel _photoMenuItem = new ShellMenuItemViewModel(typeof(PhotoViewModel)) { Name = "Photos" };
-        private readonly ShellMenuItemViewModel _videoMenuItem = new ShellMenuItemViewModel(typeof(VideoViewModel)) { Name = "Videos" };
-
-        #endregion
-
-        #region Properties
-
-        public ObservableCollection<ShellMenuItemViewModel> MenuItems { get; set; }
-        public ShellMenuItemViewModel SelectedTab
-        {
-            get => _selectedTab;
-            set => this.RaiseAndSetIfChanged(ref _selectedTab, value);
-        }
-
-        #endregion
 
         public ShellViewModel(INavigationService navigation)
         {
             _navigation = navigation;
-            MenuItems = GetMenuItems();
-            this.WhenAnyValue(i => i.SelectedTab)
-                .Where(i => i != null)
-                .Select(i => Unit.Default)
-                .InvokeCommand(ReactiveCommand.CreateFromTask(OnSelectedTabChanged));
         }
 
-        private async Task OnSelectedTabChanged()
+        public async Task SetMainPage()
         {
-            switch (SelectedTab.Name)
-            {
-                case "Photos":
-                    {
-                        await _navigation.NavigateTo<PhotoViewModel>();
-                        break;
-                    }
-                case "Videos":
-                    {
-                        await _navigation.NavigateTo<VideoViewModel>();
-                        break;
-                    }
-            }
-        }
-
-        private ObservableCollection<ShellMenuItemViewModel> GetMenuItems()
-        {
-            return new ObservableCollection<ShellMenuItemViewModel>
-            {
-                _photoMenuItem,
-                _videoMenuItem,
-            };
+            await _navigation.NavigateTo<PhotoViewModel>();
         }
     }
 }
